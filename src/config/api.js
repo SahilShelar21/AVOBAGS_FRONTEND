f026abb73b26af0bc2417fc758c2bd09bd4b6048
Sahil Vijay shelar <shelarsahil121@gmail.com>
2026-02-10 11:53:58 +0530

    fix avobags css

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,5 @@
 // src/config/api.js
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "${API_BASE_URL}";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default API_BASE_URL;
